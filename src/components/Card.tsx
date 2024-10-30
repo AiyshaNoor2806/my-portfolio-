784373cf225a332b16ecd373224812371bde290b
Aiysha Noor <161074894+AiyshaNoor2806@users.noreply.github.com>
2024-10-30 10:21:08 +0500

    update

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,39 +1,39 @@
 import React from 'react'
  import Image from 'next/image';
 
-interface propsType {
+ interface propsType {
     title: string;
     desc: string;
     img: string;
     tags: string[];
-}
-
-const Card:React.FC<propsType> = ({title, desc, img, tags }) => {
-     return (
-    <div className='border border-accent w-[300px] sm:w-[350px] data-aos="fade-left"'>
-        <div>
-            <Image 
-            className='w-[300px] sm:w-[350px] h-[auto] '
-            src= {img}
+  }
+  
+  const Card: React.FC<propsType> = ({ title, desc, img, tags }) => {
+    return (
+      <div className='border border-accent w-full sm:w-[300px] lg:w-[350px] hover:shadow-lg transform hover:-translate-y-1 transition duration-300 ease-in-out'>
+        <div className='overflow-hidden'>
+          <Image
+            className='w-full h-[200px] object-cover hover:scale-105 transition-transform duration-300'
+            src={img}
             width={350}
             height={200}
             alt={title}
-            />
+          />
         </div>
-
+  
         <div className='p-4 space-y-4'>
-            <div className='text-4xl font-extralight'>{title}</div>
-            <div>{desc}</div>
-            <div>
-                {tags.map ((el) => (
-                    <div className='tags' key={el}>
-                        {el}
-                </div>))}
-            </div>
-            </div>
-
-    </div>
-  )
-}
-
-export default Card
+          <div className='text-2xl font-semibold'>{title}</div>
+          <div className='text-gray-600'>{desc}</div>
+          <div className='flex flex-wrap gap-2'>
+            {tags.map((tag) => (
+              <div className='tags' key={tag}>
+                {tag}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Card;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,24 +5,31 @@ const Hero = () => {
   return (
     <div
       id="hero"
-      className="min-h-screen w-full mt-20 ml-20 bg-no-repeat bg-[url(../../public/my-profile.png)] bg-cover"
-      style={{
-        backgroundSize: "20%",
-        backgroundPosition: "left 100px top 100px",
+      className="min-h-screen w-full mt-20 ml-20 flex flex-col lg:flex-row items-center lg:items-start lg:space-x-8"
+     >
+      <img
+      src="/my-profile.png"
+      alt="My Profile"
+      className="w-1/4 lg:w-1/5 object-cover"
+      style={{ 
+        maxWidth: "20%",
+        marginTop: "10px",
+        marginLeft: "100px",
+        
       }}
-    >
-      <div className="container grid lg:grid-cols-2 h-[calc(100vh-60px)]">
-        <div className="hidden lg:block"></div>
-
-        <div className="flex flex-col justify-start items-center p-10 lg:pl-0 mt-20">
-          <div className="text-4xl sm:text-4xl lg:text-5xl font-bold leading-tight text-center">
-            <p data-aos="fade-left" p-y2>
-              I am Aiysha Noor
-            </p>
-            <p data-aos="fade-left">Alima Hafiza and</p>
-            <p data-aos="fade-left">now a web developer also</p>
-          </div>
+    />
+     {/* Text Section */}
+     <div className="text-left flex flex-col justify-self-start p-2 lg:p-0 mt-10 lg:mt-36">
+        <div className="text-4xl sm:text-4xl lg:text-5xl font-semibold leading-tight">
+          <p className="text-pretty" data-aos="fade-left">I am Aiysha Noor</p>
         </div>
+
+        <p
+          className="text-pretty text-purple-400 mt-2 max-w-lg leading-relaxed"
+          data-aos="fade-left"
+        >
+          I have a solid foundation in web development, specializing in HTML, CSS, and JavaScript. My experience extends to using frameworks like React and Next.js to create dynamic and user-friendly applications. I am also proficient in teaching the Quran e Kareem (physically and online). I stay updated on the latest technologies to enhance my skill set and contribute effectively to projects.
+        </p>
       </div>
     </div>
   );
